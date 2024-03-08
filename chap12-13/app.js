ff04f9e8fc8fa667ae403067a9baae4cfe708da5
Syedhadi4043 <154673334+Syedhadi4043@users.noreply.github.com>
2024-03-09 01:02:48 +0500

    chap5,12-13 done

---
+++ chap12-13/app.js
@@ -0,0 +1,17 @@
+var num1 =+prompt("Enter 1st Integer")
+var num2 =+prompt("Enter 2st Integer")
+
+if (num1>num2){
+    document.write ( num1 +" is greater than "+num2)
+}
+else if (num1=num2) { document.write( num1 +" is equal to " +num2)}
+else{ document.write (num2+ " is greater than " + num1)}
+
+
+var num3 =+prompt("Enter the  Number")
+
+
+if (num3>0){ document.write ( "Positive")}
+
+else if (num3=0) { document.write ( "Zero")}
+else ( num3<0) { document.write ("Negative")}
---
+++ chap5/app.js
@@ -0,0 +1,37 @@
+var num1 =3
+var num2 =6
+var sum1 =num1+num2
+document.write( "Sum of 3 and 6 is " + sum1 )
+
+
+
+var num3 =5
+var num4 =5
+var sum2 =num3*num4
+document.write( "Multiply of 5 and 5 is " + sum2)
+
+
+var num5  =25
+var num6 =10
+var sum3 =num5-num6
+document.write( "Substraction of 25 and 10 is " + sum3)
+
+var num7  =25
+var num8 =5
+var sum4 =num7/num8
+document.write( "Division of 25 by 5 is " + sum4)
+
+
+var num9  =8
+var num10 =8
+var sum5 =num9*num10
+document.write( " square of 8 is " + sum5)
+
+
+var math = 80
+var urdu = 78
+var eng = 84
+var chem = 42
+var phys = 75
+var total = (math+urdu+eng+chem+phys)/500*100
+document.write("Your number is;\n Maths:80/100\n Urdu:78/100\n English84/100\n Chemistry:42/100\n Physics:75/100\n Your PERCENTAGE is:   " + total)
