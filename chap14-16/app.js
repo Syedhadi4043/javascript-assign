524a1c01edab2b0335f506cbf28aa1618b12ab62
Syedhadi4043 <154673334+Syedhadi4043@users.noreply.github.com>
2024-03-09 01:37:04 +0500

    chap14-16done

---
+++ chap14-16/app.js
@@ -0,0 +1,20 @@
+var array = [ ]
+array.push ( 'pencil' ,'eraser', 'scale', 'lunch box', 'bottle' )
+console.log ( 'after push-->', array)
+
+
+array.pop ( 'bottle')
+console.log ( 'after pop-->', array)
+
+
+array.unshift ( 'school bag')
+console.log ( 'after unshift-->', array)
+
+array.shift ( 'pencil')
+console.log ( 'after shift-->', array)
+
+
+
+var fruits =[ 'mango' ,' apple', ' banana','grapes', 'peach']
+console.log ( fruits.join (','))
+console.log( fruits.length)
